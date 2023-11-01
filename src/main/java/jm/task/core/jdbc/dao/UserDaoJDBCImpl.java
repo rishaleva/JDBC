dd9c59af45bd17ec6dd98b1d101f2323455c893c
rishaleva <rishaleva@yandex.ru>
2023-11-01 22:29:48 +0300

    with transaction

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,117 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
-
-    }
+    private static final Connection connection = Util.getConnection();
 
     public void createUsersTable() {
+        String createTable = "CREATE TABLE IF NOT EXISTS user" +
+                "(id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(45), " +
+                "lastName VARCHAR(45), age TINYINT)";
+        try (connection) {
+            connection.setAutoCommit(false);
 
+            PreparedStatement preparedStatement = connection.prepareStatement(createTable);
+            preparedStatement.executeUpdate();
+
+            connection.commit();
+        } catch (SQLException e) {
+            System.out.println("Table is`s created");
+        }
     }
+    public void dropUsersTable()  {
+        try (connection) {
+            connection.setAutoCommit(false);
 
-    public void dropUsersTable() {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("DROP TABLE IF EXISTS user");
 
+            connection.commit();
+        } catch (SQLException e) {
+            System.out.println("Can`t delete table");
+        }
     }
-
     public void saveUser(String name, String lastName, byte age) {
+        try {
+            connection.setAutoCommit(false);
 
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "INSERT INTO user (name, lastName,age)" + "VALUES (?,?,?)");
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+
+            connection.commit();
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException e1) {
+                System.out.println("User is not saved");
+            }
+        }
     }
 
     public void removeUserById(long id) {
 
+        try (connection) {
+            connection.setAutoCommit(false);
+
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "DELETE FROM user WHERE id = ?");
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+
+            connection.commit();
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException e1) {
+                System.out.println("User is not removed");
+            }
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> userList = new ArrayList<>();
+        try (connection) {
+            ResultSet resultSet = connection.createStatement().executeQuery
+                    ("SELECT * FROM user");
+            while (resultSet.next()) {
+                long id = resultSet.getLong("id");
+                String name = resultSet.getString("name");
+                String lastName = resultSet.getString("lastName");
+                byte age = resultSet.getByte("age");
+                User user = new User(id, name, lastName, age);
+                userList.add(user);
+            }
+        } catch (SQLException e) {
+            System.out.println("Ошибка");
+        }
+        return userList;
     }
 
-    public void cleanUsersTable() {
+    public void cleanUsersTable()  {
+        try (connection) {
+            connection.setAutoCommit(false);
 
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("TRUNCATE TABLE user");
+
+            connection.commit();
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                System.out.println("Table not cleaned");
+            }
+        }
     }
 }
+
--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -18,11 +18,8 @@ public class User {
     @Column
     private Byte age;
 
-    public User() {
-
-    }
-
-    public User(String name, String lastName, Byte age) {
+    public User(long id,String name, String lastName, Byte age) {
+        this.id = id;
         this.name = name;
         this.lastName = lastName;
         this.age = age;
@@ -58,5 +55,15 @@ public class User {
 
     public void setAge(Byte age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
